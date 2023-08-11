b03e67e6cb57b2f58aafea4300f9fb263bee0358
SkSayan <sislam_be21@thapar.edu>
2023-08-11 21:39:30 +0530

    finished core.ts and submit errors resolved

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -35,6 +35,7 @@ interface Props {
 }
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const [files, setFiles] = useState<File[]>([])
+    const {startUpload} = useUploadThing("media")
 
     const form = useForm({
         resolver: zodResolver(UserValidation), defaultValues: {
@@ -66,13 +67,19 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         }
     }
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
+    const onSubmit= async (values: z.infer<typeof UserValidation>) => {
         const blob = values.profile_photo;
 
         const hasImageChanged = isBase64Image(blob);
 
         if(hasImageChanged){
-            
+            const imgRes = await startUpload(files)
+
+            if(imgRes && imgRes[0].fileUrl){
+                values.profile_photo = imgRes[0].fileUrl;
+            }
+        } 
+        
         
     }
 
